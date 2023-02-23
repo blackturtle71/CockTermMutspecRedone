5321b496172897945706b329993f37d06e3575a7
blackturtle71 <glebosadchiy71@gmail.com>
2023-02-23 22:23:07 +0200

    changed naming

--- scripts/ForDolphin/extract_midori_species.py
+++ scripts/ForDolphin/extract_midori_species.py
@@ -7,7 +7,7 @@ PATH_TO_MIDORI = '/home/gab/Documents/lab/TermitesAndCockroaches/MIDORI/CO1_long
 df = pd.read_csv(PATH_TO_TABLE)
 
 df = df[df.Orders == 'Blattodea_85823']
-df = df[df.CO1 >= 20] #should use 10, but we need to get rid of some more species
+df = df[df.CO1 >= 20] #should use 10, but we need to get rid of a tad more species
 
 species_list = list(df['Species'])
 ter_fams = ['Termitidae_46569', 'Rhinotermitidae_36985', 'Kalotermitidae_46562', 'Hodotermitidae_70920'] #Relevant only for midori_CO1_table.csv
@@ -19,9 +19,9 @@ for entry in SeqIO.parse(PATH_TO_MIDORI, 'fasta'):
     fam = taxonomy[5]
     if sp_name in species_list:
         if fam in ter_fams:
-            with open(f"/home/gab/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/ForDolphin/midori_separate_ids/termite/{sp_name}:{id[0]}.{id[1]}:CO1.faa", "w") as fout:
+            with open(f"/home/gab/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/ForDolphin/midori_separate_ids/termite/{sp_name}_CO1.fasta", "w") as fout:
                 fout.write(f">{id[0]}.{id[1]} [{sp_name}]\n{entry.seq}")
         else:
-            with open(f"/home/gab/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/ForDolphin/midori_separate_ids/non-termite/{sp_name}:{id[0]}.{id[1]}:CO1.faa", "w") as fout:
+            with open(f"/home/gab/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/ForDolphin/midori_separate_ids/non-termite/{sp_name}_CO1.fasta", "w") as fout:
                 fout.write(f">{id[0]}.{id[1]} [{sp_name}]\n{entry.seq}")
             
