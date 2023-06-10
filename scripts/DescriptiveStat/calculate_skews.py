81755fb0f7f5b6683ee8591d579326c897569cb3
Gleb <glebosadchiy71@gmail.com>
2023-06-10 16:51:21 +0200

    update code for using midori data

--- scripts/DescriptiveStat/calculate_skews.py
+++ scripts/DescriptiveStat/calculate_skews.py
@@ -1,39 +1,40 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-PATH_TO_COCK = '/home/gab/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/DescriptiveStat/codonusage_table_cock.csv'
-PATH_TO_TERM = '/home/gab/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/DescriptiveStat/codonusage_table_term.csv'
-
+PATH_TO_BLATTODEA = '/home/gabs/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/DescriptiveStat/codontable_midori_blattodea.csv'
 
 def get_skew_df(codon_table):
     codon_table = pd.read_csv(codon_table)
+    
+    ###ADD IT TO SEPARATE SUBSOCIAL CRIPTOCERCUS
+    codon_table.loc[codon_table["Taxonomy"] == "['Eukaryota_2759', 'Arthropoda_6656', 'Insecta_50557', 'Blattodea_85823', 'Cryptocercidae_36982']",
+                                    "Workers"] = 'Sub'
+    ###
+
     GAskew = (codon_table['nG'] - codon_table['nA'])/(codon_table['nG'] + codon_table['nA'])
-    TCskew = (codon_table['nC'] - codon_table['nT'])/(codon_table['nC'] + codon_table['nT'])
+    TCskew = (codon_table['nT'] - codon_table['nC'])/(codon_table['nT'] + codon_table['nC'])
     Stg_Sac = (codon_table['neutralT'] + codon_table['neutralG']) - (codon_table['neutralA'] + codon_table['neutralC'])
     IDs = codon_table['Species_name']
     genes = codon_table['Gene_name']
+    workers = codon_table['Workers']
 
     skews = IDs.to_frame().merge(GAskew.rename('GAskew'), left_index=True, right_index=True)
     skews = skews.merge(genes.rename('Gene_name'), left_index=True, right_index=True)
     skews = skews.merge(TCskew.rename('TCskew'), left_index=True, right_index=True)
     skews = skews.merge(Stg_Sac.rename('Stg-Sac'), left_index=True, right_index=True)
+    skews = skews.merge(workers.rename('Organism'), left_index=True, right_index=True)
 
     return (skews)
 
 
-cock_skew = get_skew_df(PATH_TO_COCK)
-term_skew = get_skew_df(PATH_TO_TERM)
+
+blattodea_skew = get_skew_df(PATH_TO_BLATTODEA)
+blattodea_skew = blattodea_skew.sort_values(by=['Organism', 'Species_name'], ascending=True)
+
+blattodea_skew['Organism'] = blattodea_skew['Organism'].map({1.0 : 'Termites w/ workers', 0.0 : 'Termites w/o workers', "Sub" : 'Sub-social Cryptocercus'}) ### LAST ONE SEPARATES SUBSOCIAL SPECIES
+blattodea_skew['Organism'].fillna('Cockroaches', inplace=True)
 
 
-Organism = pd.Series(["Cockroach"]*len(cock_skew))
-cock_skew = cock_skew.merge(Organism.rename('Organism'), left_index=True, right_index=True)
-
-Organism = pd.Series(["Termite"]*len(term_skew))
-term_skew = term_skew.merge(Organism.rename('Organism'), left_index=True, right_index=True)
-
-
-skews = pd.concat([cock_skew, term_skew])
-skews.to_csv("/home/gab/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/DescriptiveStat/cock_term_skews.csv", na_rep='NA')
+blattodea_skew.to_csv("/home/gabs/Documents/lab/TermitesAndCockroaches/MutSpec-Redone/interim/DescriptiveStat/midori_blattodea_skew.csv", na_rep='NA')
 
 
 #cock_skew.to_csv("../../interim/DescriptiveStat/cock_skew.csv", na_rep='NA')
